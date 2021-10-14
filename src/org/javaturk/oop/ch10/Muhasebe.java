c0f91bc0b03b34f2168e2b11c130185a9d200d01
Serhat Topcu <codearts@icloud.com>
2021-10-14 12:35:47 +0300

    file to write

--- src/org/javaturk/oop/ch10/Muhasebe.java
+++ src/org/javaturk/oop/ch10/Muhasebe.java
@@ -1,10 +1,32 @@
 package org.javaturk.oop.ch10;
 
-import java.util.EmptyStackException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Muhasebe {
-    public static void main(String[] args) {
+
+    public static Double  kontrol(Scanner input) {
+        do {
+            try {
+                return Double.parseDouble(input.next());
+            }catch (NumberFormatException ex){
+                System.out.println("Double deger giriniz." + ex);
+            }
+        }while(true);
+    }
+
+    public static void fileSave(String data) throws IOException {
+        String content = data;
+        String path = "D:/java/Java2/text/muhasebe.txt";
+        Files.write( Paths.get(path), content.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public static void main(String[] args) throws IOException {
+        double a = gToplam();
+        fileSave("Toplam Giderleriniz : " + String.valueOf(a) + " azaltmalisin");
 
         if(gToplam() < 1000){
             System.out.println("Giderleriniz tutumlu");
@@ -12,48 +34,46 @@ public class Muhasebe {
             System.out.println("Toplam Giderleriniz : " + gToplam() + " azaltmalisin");
         }
     }
-
     public static double setEnerji(){
         Scanner input  = new Scanner(System.in);
         System.out.print("Suyu girin : ");
-        double s = input.nextDouble();
+        double s = kontrol(input);
         System.out.print("Elektrik girin : ");
-        double e = input.nextDouble();
+        double e = kontrol(input);
         System.out.print("Telekom girin : ");
-        double t = input.nextDouble();
+        double t = kontrol(input);
         System.out.print("Dogalgaz girin : ");
-        double d = input.nextDouble();
+        double d = kontrol(input);
+
         Enerji enerji = new Enerji();
         double total = enerji.getEnerji(s,e,t,d);
         return total;
     }
-
     public static double setGida(){
         Scanner input  = new Scanner(System.in);
         System.out.print("Market girin : ");
-        double a = input.nextDouble();
+        double a = kontrol(input);
         System.out.print("Pazar girin : ");
-        double b = input.nextDouble();
+        double b = kontrol(input);
         System.out.print("DisMekan girin : ");
-        double c = input.nextDouble();
+        double c = kontrol(input);
         Gida enerji = new Gida();
         double total = enerji.getGida(a,b,c);
         return total;
     }
-
     public static double setAbonelikler(){
         Scanner input  = new Scanner(System.in);
         System.out.print("Aidat girin : ");
-        double a = input.nextDouble();
+        double a = kontrol(input);
         System.out.print("Kira girin : ");
-        double b = input.nextDouble();
+        double b = kontrol(input);
         System.out.print("Online Uyelik masraflarini girin : ");
-        double c= input.nextDouble();
+        double c= kontrol(input);
         Abonelikler abone = new Abonelikler();
         double total = abone.getAbonelikler(a,b,c);
         return total;
-    }
 
+    }
     public static double gToplam(){
         var abonelik = setAbonelikler();
         System.out.println("Toplam Abonelik Gideriniz : " + abonelik);
